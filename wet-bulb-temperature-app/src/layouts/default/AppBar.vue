dd275ffaf9c6de43788231723540938b838ff947
Chris Gravatt <chrisgravatt@gravmac.lan>
2023-03-30 13:41:29 -0400

    Adds scaffolding for temperature unit switching, buttons are non-functional at this point though

--- wet-bulb-temperature-app/src/layouts/default/AppBar.vue
+++ wet-bulb-temperature-app/src/layouts/default/AppBar.vue
@@ -1,9 +1,13 @@
 <template>
-  <v-app-bar flat class="bg-secondary elevation-3 justify-center custom-app-bar">
+  <v-app-bar flat class="bg-secondary elevation-3 d-flex align-center justify-between custom-app-bar">
     <v-app-bar-title style="color: white">
       <v-icon icon="mdi-sun-thermometer-outline" />
-      Find Local Wet Bulb Temperature
+      Find Your Local Wet Bulb Temperature
     </v-app-bar-title>
+    <template v-slot:append>
+      <v-btn icon="mdi-temperature-celsius"></v-btn>
+      <v-btn icon="mdi-temperature-fahrenheit"></v-btn>
+    </template>
   </v-app-bar>
 </template>
 
