501ec2f6f0916fa0681b74ecd0421fb70a1d80e0
Chris Gravatt <chrisgravatt@gravmac.lan>
2023-04-03 16:56:31 -0400

    beginning styling of learn more page; WIP

--- wet-bulb-temperature-app/src/components/LearnMorePage.vue
+++ wet-bulb-temperature-app/src/components/LearnMorePage.vue
@@ -1,6 +1,21 @@
 <template>
+  <v-app :style="{ background: bgColor, 'background-attachment': 'fixed', 'overflow-y': 'hidden', 'overscroll-behavior-y': 'none'}">
+    <v-container class="fill-height">
+      <div class="d-flex text-h6 align-center mb-12 justify-center" style="color: white">
+        Are you wondering just wtf a "wet bulb" temperature is? You've come to the right place.
+      </div>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-    //
+  import { useDynamicBackgroundStore } from '@/store/app.js';
+
+  export default {
+  computed: {
+      bgColor() {
+      return useDynamicBackgroundStore().getBackgroundColor;
+      },
+    },
+  };
 </script>
